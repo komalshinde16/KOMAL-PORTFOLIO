7a189b14bf35940939b28d234bea170c831b445a
komalshinde16 <komalambadasshinde@gmail.com>
2026-06-13 14:53:25 +0530

    created hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
+import Hero from "./components/Hero";
+
 function App() {
   return (
-    <div>
-      <h1>Komal Shinde</h1>
-      <h2>Full Stack Java Developer</h2>
-      <p>Electronics & Telecommunication Engineer </p>
-    </div>
+    <>
+      <Hero />
+    </>
   );
 }
 
---
+++ src/components/Hero.jsx
@@ -0,0 +1,10 @@
+const Hero = () => {
+  return (
+    <section>
+      <h1>Komal Shinde</h1>
+      <h2>Full Stack Java Developer | Electronics & Telecommunication Engineer</h2>
+    </section>
+  );
+};
+
+export default Hero;
