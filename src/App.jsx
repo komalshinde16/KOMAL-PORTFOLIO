0c770052d3f547fd1ccd729afc1e063106f5beda
komalshinde16 <komalambadasshinde@gmail.com>
2026-06-13 14:45:24 +0530

    intial react setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1>Komal Shinde</h1>
+      <h2>Full Stack Java Developer</h2>
+      <p>Electronics & Telecommunication Engineer </p>
+    </div>
+  );
+}
+
+export default App;
